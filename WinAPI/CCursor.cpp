09bc3b23bb6cf282cdb850ea8cfeafc9d88bbab9
seung <kumbung01@naver.com>
2024-11-12 05:35:41 +0900

    커서에 텍스쳐 추가

--- WinAPI/CCursor.cpp
+++ WinAPI/CCursor.cpp
@@ -3,6 +3,8 @@
 #include "CCamera.h"
 #include "CDbgRender.h"
 #include "CKeyMgr.h"
+#include "CEngine.h"
+#include "CAssetMgr.h"
 
 CCursor::CCursor()
 	: m_Tex(nullptr)
@@ -18,6 +20,7 @@ CCursor::~CCursor()
 void CCursor::Begin()
 {
 	// 커서 텍스쳐 렌더
+	m_Tex = CAssetMgr::GetInst()->LoadTexture(L"Cursor", L"Texture\\cursor.png");
 }
 
 void CCursor::Tick()
@@ -35,4 +38,15 @@ void CCursor::Render()
 					GetRenderPos(),
 					GetScale(), 
 					0.f);
+
+	auto graphics = CEngine::GetInst()->GetBackGraphics();
+	Vec2 vPos = GetRenderPos();
+
+	float centerX = vPos.x - m_Tex->GetWidth() / 2.f;
+	float centerY = vPos.y - m_Tex->GetHeight() / 2.f;
+	
+	graphics->DrawImage(m_Tex->GetImage(),
+		(int)centerX, (int)centerY,
+		(int)0, (int)0,
+		m_Tex->GetWidth(), m_Tex->GetHeight(), UnitPixel);
 }
